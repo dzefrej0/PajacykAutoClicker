ded01cb8bdab8a9055139d8012e93b690c183726
dzefrej0 <mchpiechota@gmail.com>
2021-06-28 19:22:21 +0200

    added some methods to show how to work with scanner class Added txt filet o work on with scanner class refactored code

--- src/main/java/pajacyk/PajacykMain.java
+++ src/main/java/pajacyk/PajacykMain.java
@@ -4,45 +4,86 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.io.*;
 import java.sql.Time;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Scanner;
 
 public class PajacykMain {
 
     WebDriver driver = new ChromeDriver();
+    private File fileToSaveData;
 
     PajacykMain(){
         System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
         driver.navigate().to("https://www.pajacyk.pl");
     }
 
-    public void makeAClickOnPajacyk (){
+    private String getUserInputFromPrompt (){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Write down ur name plz- ");
+        String a= scanner.next();
+        System.out.println("ur name is : " + a);
+        return a;
+    }
+
+    private void saveUserNameToFile (String StringToSave) throws IOException {
+
+        try (FileWriter fileWriter = new FileWriter("test.txt")) {
+            fileWriter.write(StringToSave);
+        }
+    }
+
+    private void checkIfFileExistAndIfNoMakeOne () throws IOException {
+        fileToSaveData = new File("test.txt");
+        boolean fileExists = fileToSaveData.exists();
+        System.out.println("attempt to make a file");
+        if(!fileExists){
+            FileWriter fileWriter = new FileWriter("test.txt");
+            System.out.println("there was no file but now its made");
+        }else{
+            System.out.println("there is file already");
+        }
+    }
+
+    private void makeAClickOnPajacyk (){
         driver.findElement(By.xpath("/html/body/div[2]/section/div[5]")).click();
     }
 
-    public void closeConnection (){
+    private void closeConnection (){
         driver.close();
     }
 
-    public void getCountOfClicks (){
+    private void getCountOfClicks (){
 
         String a = driver.findElement(By.xpath("//html/body/div[2]/section/div[6]/div[1]")).getText();
         System.out.println(a);
     }
 
-    public LocalDateTime getTimeDateNow (){
+    private LocalDateTime getTimeDateNow (){
         LocalDateTime localDateTime = LocalDateTime.now();
 
         return localDateTime;
     }
 
-    public static void main(String[] args) throws InterruptedException {
+    private static void readFileContents() throws IOException {
+        System.out.println("Reading contents of file using Scanner class:");
+        try (FileReader fileReader = new FileReader("test.txt");
+             Scanner scanner=new Scanner(fileReader)){
+            while (scanner.hasNext()) {
+                System.out.println("zawartość pliku :  " + scanner.next());
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException, IOException {
         System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-
         PajacykMain pajacykMain = new PajacykMain();
+        pajacykMain.checkIfFileExistAndIfNoMakeOne();
+        pajacykMain.saveUserNameToFile(pajacykMain.getUserInputFromPrompt());
+        pajacykMain.readFileContents();
         for (int i = 0; i < 10000; i++) {
-
             System.out.println("ilość naszych kliknięć:     "+ i );
             System.out.println("czas kliknięcia  : " + pajacykMain.getTimeDateNow());
             pajacykMain.makeAClickOnPajacyk();
@@ -50,7 +91,6 @@ public class PajacykMain {
             pajacykMain.getCountOfClicks();
             Thread.sleep(10000);
         }
-
         pajacykMain.closeConnection();
     }
 }
